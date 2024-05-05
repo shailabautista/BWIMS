d1e7b757610f2d05ed4dfd557835650a0fdab1c2
shailabautista <shailamae.bautista16@gmail.com>
2024-05-05 14:35:26 +0800

    maayos ka na

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -79,7 +79,7 @@ const Header = () => {
       <Row className="d-flex justify-content-between align-items-center gap-2">
         <Col className="d-flex align-items-center">
           <img src={Logo} alt="logo" width={100} />
-          {filteredBarangayData && filteredBarangayData[0].logo && barangay &&(
+          {filteredBarangayData && filteredBarangayData.length > 0 && filteredBarangayData[0].logo && myBarangay && (
             <img
               src={
                 filteredBarangayData[0].logo ||
