a130b46794bfd7f4926fba6b209f90ca6ed4257c
shailabautista <152846962+shailabautista@users.noreply.github.com>
2024-05-15 19:06:41 +0800

    Update ViewBarangayClearancePage.jsx

--- src/pages/ViewPage/ViewBarangayClearancePage.jsx
+++ src/pages/ViewPage/ViewBarangayClearancePage.jsx
@@ -90,14 +90,14 @@ const PDFDocument = ({ formData, barangay }) => (
       </Text>{" "}
       <Text style={{
           marginBottom: 20,
-          paddingLeft: '1.7cm',
-          paddingRight: '2cm',
+          paddingLeft: '2cm',
+          paddingRight: '2.5cm',
         }}>
         TO WHOM IT MAY CONCERN:</Text>
       <Text style={{
           marginBottom: 20,
-          paddingLeft: '1.7cm',
-          paddingRight: '2cm'',
+          paddingLeft: '2cm',
+          paddingRight: '2.5cm'',
         }}>
         {" "}{" "}{" "}{" "}{" "}{" "}This is to certify that {formData.lastName},
         {formData.firstName} {formData.middleName} male/female, married/single
@@ -107,24 +107,24 @@ const PDFDocument = ({ formData, barangay }) => (
       </Text>
       <Text style={{
           marginBottom: 20,
-          paddingLeft: '1.7cm',
-          paddingRight: '2cm',
+          paddingLeft: '2cm',
+          paddingRight: '2.5cm',
         }}>
         It is futher certified that above-named person has never 
         been accused in any crime.
       </Text>
       <Text style={{
           marginBottom: 20,
-          paddingLeft: '1.7cm',
-          paddingRight: '2cm',
+          paddingLeft: '2cm',
+          paddingRight: '2.5cm',
         }}>
         This certification is issued upon request for whatever legal purpose
         and intends it may serve.
       </Text>
       <Text style={{
           marginBottom: 20,
-          paddingLeft: '1.7cm',
-          paddingRight: '2cm',
+          paddingLeft: '2cm',
+          paddingRight: '2.5cm',
         }}>
         Issued {new Date(formData.date).toLocaleString()} at {barangay} Barangay Hall, Dagupan City.
       </Text>
