fdeaf6a086cf29bab0f123c07643ead08d151de0
shailabautista <152846962+shailabautista@users.noreply.github.com>
2024-05-15 15:57:26 +0800

    Update ViewCerificateOfIndigencyPage.jsx

--- src/pages/ViewPage/ViewCerificateOfIndigencyPage.jsx
+++ src/pages/ViewPage/ViewCerificateOfIndigencyPage.jsx
@@ -39,6 +39,7 @@ const PDFDocument = ({ formData, barangay }) => (
         style={{
           textAlign: "center",
           fontSize: 12,
+          marginTop: 20,
           marginBottom: 2,
         }}
       >
