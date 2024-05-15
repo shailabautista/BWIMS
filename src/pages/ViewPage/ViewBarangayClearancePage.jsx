11b4a1104c8b41ab3bbf880edc6fd215e35c31b2
shailabautista <152846962+shailabautista@users.noreply.github.com>
2024-05-15 15:01:57 +0800

    Update ViewBarangayClearancePage.jsx

--- src/pages/ViewPage/ViewBarangayClearancePage.jsx
+++ src/pages/ViewPage/ViewBarangayClearancePage.jsx
@@ -95,7 +95,7 @@ const PDFDocument = ({ formData, barangay }) => (
       <Text style={{
           marginBottom: 20,
         }}>
-        {" "}{" "}{" "}{" "}{" "}{" "}This is to certify that {formData.lastName},
+        {" "}{" "}{" "}This is to certify that {formData.lastName},
         {formData.firstName} {formData.middleName} male/female, married/single
         of legal age, Filipino and a resident of Barangay {barangay},
         Dagupan City, is known me personally to be a good moral character and a law 
