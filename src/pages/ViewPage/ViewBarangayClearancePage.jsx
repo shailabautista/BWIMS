996951cc0ca2257baa9c2209fe604daaccf6234e
shailabautista <152846962+shailabautista@users.noreply.github.com>
2024-05-20 23:56:05 +0800

    Update ViewBarangayClearancePage.jsx

--- src/pages/ViewPage/ViewBarangayClearancePage.jsx
+++ src/pages/ViewPage/ViewBarangayClearancePage.jsx
@@ -132,7 +132,7 @@ const PDFDocument = ({ formData, barangay }) => (
       </Text>
       <Text style={{
           textAlign: "center",
-          paddingLeft: '13cm',
+          paddingLeft: '12cm',
           paddingRight: '0cm',
         }}>
         Barangay Captain
