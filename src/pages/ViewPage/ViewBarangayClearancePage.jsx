408b1cacf98c5ed1adf1e4d746431d4e9325bb54
shailabautista <152846962+shailabautista@users.noreply.github.com>
2024-05-21 00:20:18 +0800

    Update ViewBarangayClearancePage.jsx

--- src/pages/ViewPage/ViewBarangayClearancePage.jsx
+++ src/pages/ViewPage/ViewBarangayClearancePage.jsx
@@ -100,7 +100,7 @@ const PDFDocument = ({ formData, barangay }) => (
           paddingRight: '2cm',
         }}>
         {" "}{" "}{" "}{" "}{" "}{" "}{" "}This is to certify that{" "}
-        <Text style={{ fontSize: 12, fontWeight: 5, }}>{formData.lastName}, {formData.firstName} {formData.extensionName ? formData.extensionName : null} {formData.middleName}
+        <Text style={{ fontSize: 12, fontWeight: 20, }}>{formData.lastName}, {formData.firstName} {formData.extensionName ? formData.extensionName : null} {formData.middleName}
         </Text>
         {" "} male/female, married/single
         of legal age, Filipino and a resident of Barangay {barangay},
@@ -128,7 +128,7 @@ const PDFDocument = ({ formData, barangay }) => (
           textAlign: "right",
           paddingLeft: '2cm',
           paddingRight: '2cm',
-          fontWeight: 10,
+          fontWeight: 20,
         }}>
         {barangay === "Salapingao" ? "DELFIN C. DE GUZMAN" : barangay === "Lomboy" ? "ARSENIO V. SANTILLAN SR." : 'Kapitan C. Tutan'}
       </Text>
